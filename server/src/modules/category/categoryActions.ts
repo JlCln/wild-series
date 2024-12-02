7893a34de31b6d1b08b806fa0533ff846cff54ec
JlCln <j.coulon.pro@gmail.com>
2024-12-02 16:34:01 +0100

    finished repository quest

--- server/src/modules/category/categoryActions.ts
+++ server/src/modules/category/categoryActions.ts
@@ -1,3 +1,6 @@
+// Import access to data
+import categoryRepository from "./categoryRepository";
+
 // Some data to make the trick
 
 const categories = [
@@ -15,8 +18,10 @@ const categories = [
 
 import type { RequestHandler } from "express";
 
-const browse: RequestHandler = (req, res) => {
-  res.json(categories);
+const browse: RequestHandler = async (req, res) => {
+  const categoriesFromDB = await categoryRepository.readAll();
+
+  res.json(categoriesFromDB);
 };
 
 const read: RequestHandler = (req, res) => {
